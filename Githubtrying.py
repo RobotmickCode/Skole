18bc4223beb1c7f929d96ce8b05c33fbd224afac
Robotmick <RobotmickCode@gmail.com>
2026-08-10 15:17:59 +0200

    name

--- Githubtrying.py
+++ Githubtrying.py
@@ -1,2 +1,5 @@
 print("Hello World!")
 # Waaaaasaaaaaa uuuppp 
+
+name = input("Hello traveler what is your name?: ")
+print(f"Ah what a most marvoulys name. {name}! Your story shall be told for generations")
