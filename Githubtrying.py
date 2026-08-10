1079e39c9a4da66773ef9979c5c32f0aa4744e96
Robotmick <RobotmickCode@gmail.com>
2026-08-10 15:20:21 +0200

    mage branch???

--- Githubtrying.py
+++ Githubtrying.py
@@ -3,3 +3,4 @@ print("Hello World!")
 
 name = input("Hello traveler what is your name?: ")
 print(f"Ah what a most marvoulys name. {name}! Your story shall be told for generations")
+print("So traveller do u wish to me a mage?")
