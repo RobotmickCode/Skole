e80c640254bcb5324bda7af165ff8251dc94387a
Robotmick <RobotmickCode@gmail.com>
2026-08-10 15:29:20 +0200

    class_choice

--- Githubtrying.py
+++ Githubtrying.py
@@ -3,4 +3,12 @@ print("Hello World!")
 
 name = input("Hello traveler what is your name?: ")
 print(f"Ah what a most marvoulys name. {name}! Your story shall be told for generations")
-print("So traveller do u wish to me a mage?")
+print("So traveller do u wish to me a mage or a warrior as your class?") 
+class_choice = input("Type 'mage' or 'warrior': ") 
+if class_choice.lower() == "mage":
+    print("So you choce the path pf a mage. Glass canons or support for your team")
+else:
+    print("So you choce the path of a warrior. A tanky frontline or a dps for your team")
+
+
+
